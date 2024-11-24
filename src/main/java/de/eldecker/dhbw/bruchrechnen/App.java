f78bfab8f7b5b7473fa09cb3e075211e714fb9df
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-11-24 14:42:58 +0100

    Klasse Bruch dazu

---
+++ src/main/java/de/eldecker/dhbw/bruchrechnen/App.java
@@ -0,0 +1,10 @@
+package de.eldecker.dhbw.bruchrechnen;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        System.out.println( "Hallo DHBW" );
+    }
+    
+}
---
+++ src/main/java/de/eldecker/dhbw/bruchrechnen/Bruch.java
@@ -0,0 +1,212 @@
+package de.eldecker.dhbw.bruchrechnen;
+
+
+/**
+ * Klasse für Bruchrechnen.
+ */
+class Bruch {
+
+    /** Zähler des Bruchs. */
+    private int zaehler;
+    
+    /** Nenner des Bruchs, darf nicht 0 sein. */
+    private int nenner;
+
+
+    /**
+     * Bruch nur mit Zähler erzeugen (Nenner ist 1).
+     * 
+     * @param zaehler Zähler-Wert
+     */
+    public Bruch( int zaehler ) {
+
+        this( zaehler, 1 );
+    }
+
+    
+    /**
+     * Konstruktor, um neuen Bruch mit Zähler und Nenner erzeugen.
+     *
+     * @param zaehler Zähler-Wert
+     *
+     * @param nenner Nenner, darf nicht 0 sein
+     */
+    public Bruch( int zaehler, int nenner ) {
+
+          this.zaehler = zaehler;
+          this.nenner  = nenner;
+    }
+
+
+    /**
+     * Gibt gekürzten Bruch des aufgerufenen Objekts zurück,
+     * z.B. {@code 1/2} für {@code 2/4}.
+     *
+     * @return Gekürzter Bruch
+     */
+    public Bruch kuerze() {
+
+        int ggt = ggt( zaehler, nenner );
+
+        int zaehlerNeu = zaehler / ggt;
+        int nennerNeu  = nenner  / ggt;
+
+        return new Bruch( zaehlerNeu, nennerNeu );
+    }
+
+
+    /**
+     * Kehrwert des Bruchs zurückgeben, bspw. {@code 2/3} für {@code 3/2}.
+     *
+     * @return Kehrwert (Zähler und Nenner vertauscht)
+     */
+    public Bruch kehrwert() {
+
+        return new Bruch( nenner, zaehler );
+    }
+
+
+    /**
+     * Negiert den Bruch.
+     * <br>
+     * 
+     * Beispiel: der Kehrwert von {@code 3/4} ist {@code 4/3}.     
+     *
+     * @return Negierter Bruch
+     */
+    public Bruch negieren() {
+
+        return new Bruch( -zaehler, nenner );
+    }
+
+
+    /**
+     * Multipliziert den Bruch mit einem anderen Bruch.
+     * Rechenregel: Zähler mal Zähler, Nenner mal Nenner.
+     *
+     * @param b Bruch, mit dem multipliziert werden soll
+     *
+     * @return Gekürztes Ergebnis
+     */
+    public Bruch multiplizieren( Bruch b ) {
+
+        int zaehlerNeu = b.zaehler * this.zaehler;
+        int nennerNeu  = b.nenner  * this.zaehler;
+
+        Bruch produkt = new Bruch( zaehlerNeu, nennerNeu );
+
+        return produkt.kuerze();
+    }
+
+    
+    /**
+     * Dividiert den Bruch durch einen anderen Bruch
+     * (durch Multiplikation mit dem Kehrwert).
+     *
+     * @param b Bruch, durch den dividiert werden soll
+     *
+     * @return Gekürztes Ergebnis
+     */
+    public Bruch dividieren( Bruch b ) {
+
+        Bruch bKehrwert = b.kehrwert();
+
+        return multiplizieren( bKehrwert );
+    }
+
+
+    /**
+     * Addiert den Bruch mit einem anderen Bruch.
+     *
+     * @param b Bruch, mit dem addiert werden soll
+     *
+     * @return Gekürztes Ergebnis
+     */
+    public Bruch addieren( Bruch b ) {
+
+        int zaehlerNeu = zaehler*b.nenner + b.zaehler*nenner;
+        int nennerNeu  = nenner*b.nenner;
+
+        return new Bruch( zaehlerNeu, nennerNeu ).kuerze();
+    }
+
+
+    /**
+     * Subtrahiert den Bruch mit einem anderen Bruch.
+     *
+     * @param b Bruch, von dem subtrahiert werden soll
+     *
+     * @return Gekürztes Ergebnis
+     */
+    public Bruch subtrahieren( Bruch b ) {
+
+        Bruch bNegiert = b.negieren();
+
+        return addieren( bNegiert );
+    }
+    
+    
+    /**
+     * Dezimalwert für Bruch zurückgeben, z.B. {@code 0.5} für {@code 1/2}.
+     *
+     * @return Dezimalwert
+     */
+    public double wert() {
+
+        return (double) zaehler / nenner;
+    }    
+
+
+    /**
+     * String-Darstellung des Bruchs zurückgeben.
+     *
+     * @return String-Darstellung des Bruchs, z.B. {@code 3/4}
+     */
+    @Override
+    public String toString() {
+
+        return zaehler + "/" + nenner;
+    }
+
+
+    /**
+     * Berechnung größter gemeinsamer Teiler von {@code a} und {@code b},
+     * wird für die Kürzung von Brüchen benötigt; verwendet den
+     * Wechselwegnahme-Algorithmus, bei dem in jedem Schleifenlauf
+     * die kleinere von der größeren Zahl abgezogen wird.
+     * <br><br>
+     *
+     * Beispiel: {@code ggt( 12, 18 )} ergibt {@code 6}.
+     *
+     * @param a Erster Wert
+     *
+     * @param b Zweiter Wert
+     *
+     * @return Größter gemeinsamer Teiler
+     */
+    public static int ggt ( int a, int b ) {
+
+        if ( a == 0 ) { return b; }
+
+        if ( b == 0 ) { return a; }
+
+        int x = Math.abs( a );
+        int y = Math.abs( b );
+
+        while ( x != y ) {
+
+            if ( x > y ) {
+
+                x = x - y;
+
+            } else {
+
+                y = y - x;
+            }
+        }
+
+        return x;
+    }
+
+}
+
